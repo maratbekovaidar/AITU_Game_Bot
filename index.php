8341b111aa5db2d678008dc5b812ac5bad3fe33e
astreateam <72739461+astreateam@users.noreply.github.com>
2020-11-15 17:00:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,8 @@
 $data = json_decode(file_get_contents('php://input'), TRUE);
 file_put_contents('file.txt', '$date: '.print_r($date, 1)."\n", FILE_APPEND);
 
+//Обработка
+
+https://api.telegram.org/bot1446264305:AAH-Xk0We8VPfgCarvzSZnh2CNHDc9RSC90/setwebhook?url=https://github.com/maratbekovaidar/AITU_Game_Bot/blob/main/index.php
+
+?>
